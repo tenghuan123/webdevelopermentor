82eba7c84c527fa93ec031cfd8162eb502529e6d
玄星 <2540149177@qq.com>
2022-05-31 07:23:44 +0800

    commented gatsby-plugin-google-analytics because don't have trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,14 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-emotion", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-emotion", 
+//   {
+//     resolve: 'gatsby-plugin-google-analytics',
+//     options: {
+//       "trackingId": ""
+//     }
+//   }, 
+  "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
